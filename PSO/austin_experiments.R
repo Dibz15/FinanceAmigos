c218c179f7c31c2b2416b843cd6ea069de7ffea3
dibz15 <austindibble@pm.me>
2023-04-01 21:22:25 +0100

    Got some results, but they are bad
    - Using PSO to optimise a perceptron/linear regression model is a bad idea. Let's try something else

--- PSO/austin_experiments.R
+++ PSO/austin_experiments.R
@@ -26,7 +26,7 @@ library(tidyr)
 
 symbol <- "AMD"
 start_date <- as.Date("2022-01-01")
-end_date <- as.Date("2022-04-01")
+end_date <- as.Date("2023-01-01")
 stock_data <- getSymbols(symbol, src = "yahoo", from = start_date, to = end_date, auto.assign = FALSE)
 
 # Close price
@@ -53,15 +53,15 @@ bbands_pct <- (close_price - bbands[,1]) / (bbands[,3] - bbands[,1])
 volume = Vo(stock_data)
 
 # Merge all the features into a single dataset
-features <- merge(close_price, volume, sma, ema, rsi, macd_signal, bbands_pct)
-colnames(features) <- c("Close_Price", "Volume", "SMA", "EMA", "RSI", "MACD_Signal", "BBands_Pct")
+features <- merge(close_price, sma, ema, rsi, macd_signal, bbands_pct)
+colnames(features) <- c("Close_Price", "SMA", "EMA", "RSI", "MACD_Signal", "BBands_Pct")
 
 # Remove rows with NA values
 features_complete <- na.omit(features)
 
 # Convert xts object to data frame
 features_df <- data.frame(Date = index(features_complete), coredata(features_complete))
-colnames(features_df) <- c("Date", "Close_Price", "Volume", "SMA", "EMA", "RSI", "MACD_Signal", "BBands_Pct")
+colnames(features_df) <- c("Date", "Close_Price", "SMA", "EMA", "RSI", "MACD_Signal", "BBands_Pct")
 
 # Reshape data frame to long format
 features_long <- features_df %>%
@@ -98,8 +98,6 @@ train_data <- features_complete[1:train_size]
 validation_data <- features_complete[(train_size + 1):(train_size + validation_size)]
 test_data <- features_complete[(train_size + validation_size + 1):n_rows]
 
-
-
 # 
 # # Similar fitness function to GA
 # # But without weight normalisation
@@ -121,35 +119,27 @@ test_data <- features_complete[(train_size + validation_size + 1):n_rows]
 # PSOweights <- PSOweights / sum(PSOweights)
 # PSOweights
 
-mse_values = rep(0, 1000)
-it_value = 0
-window_size = 2 # days
-prediction_size = 2
+window_size = 25 # days
+prediction_size = 1
+num_features = ncol(train_data)
 
-mse_fitness_function <- function(weights, data) {
-  start_index <- sample(1:(nrow(data) - window_size), 1)
+mse_fitness_function <- function(weights_vector, data) {
+  weights <- matrix(weights_vector, nrow = window_size, ncol = num_features, byrow = TRUE)
+  start_index <- sample(1:(nrow(data) - window_size - prediction_size), 1)
+  
   # Extract a portion of the data with the given window size
   window_data <- data[start_index:(start_index + window_size - 1),]
-  actual_data = data[(start_index + window_size):(start_index + window_size - 1) + prediction_size,]
-  print(window_data)
-  print(actual_data)
+  actual_data = data[(start_index + window_size):(start_index + window_size - 1 + prediction_size),]
   actual_prices = actual_data[, "Close_Price"]
-  # actual_price <- window_data[-1, "Close_Price"]
-  # print(actual_price)
-  # actual_price <- data[, "Close_Price"]
-  # # print(actual_price)
-  # predicted_price <- sum(weights * data[, -1]) # -1 to exclude the Close_Price column
-  # mse <- mean((actual_price - predicted_price) ^ 2)
-  # mse_values[it_value] = mse
-  # it_value = it_value + 1
-  mse = 0
+  predicted_price = sum(window_data * weights)
+  mse = abs(actual_prices - predicted_price)
   return(mse)
 }
 
 # Set the lower and upper bounds for the weights
-lower_bounds <- rep(-1, 6) # -1 for each feature weight
-upper_bounds <- rep(1, 6)  # 1 for each feature weight
-initial_weights = runif(6, min=lower_bounds, max=upper_bounds)
+lower_bounds <- rep(-100, window_size * num_features) # -1 for each feature weight
+upper_bounds <- rep(100, window_size * num_features)  # 1 for each feature weight
+initial_weights =  runif(window_size * num_features, min = lower_bounds, max = upper_bounds)
 
 mse_fitness_function(initial_weights, train_data)
 
@@ -160,16 +150,90 @@ result <- psoptim(
   lower = lower_bounds,
   upper = upper_bounds,
   data = train_data, # Pass the train_data as additional argument to the fitness function
-  control = list(maxit = 1000, print_iter=100) # Set the maximum number of iterations, adjust as needed
+  control = list(
+    maxit = 1000, # Set the maximum number of iterations, adjust as needed
+    w = 0.72984, # Inertia weight
+    # c.p = 2.05, # Cognitive component
+    # c.g = 4.05, # Social component,
+    trace=1,
+    trace.stats=TRUE
+  )
 )
 
 # Extract the optimal weights
 optimal_weights <- result$par
 
-# Create a data frame with the iteration numbers and MSE values
-mse_data <- data.frame(Iteration = 1:length(mse_values), MSE = mse_values)
+iterations <- result$stats$it
+best_fitness_values <- result$stats$error
+fitness_data <- data.frame(Iteration = iterations, Best_Fitness = best_fitness_values)
 
-# Plot the MSE values
-ggplot(mse_data, aes(x = Iteration, y = MSE)) +
+ggplot(fitness_data, aes(x = Iteration, y = Best_Fitness)) +
   geom_line() +
-  labs(title = "MSE during PSO Training", x = "Iteration", y = "MSE")
+  theme_minimal() +
+  labs(title = "Fitness Values Over Iterations",
+       x = "Iteration",
+       y = "Best Fitness Value")
+
+
+predict_prices <- function(data, weights, window_size) {
+  num_predictions <- nrow(data) - window_size
+  predicted_prices <- rep(0, num_predictions)
+  
+  # start_index <- sample(1:(nrow(data) - window_size - prediction_size), 1)
+  
+  for (i in 1:num_predictions) {
+    window_data <- data[i:(i + window_size - 1), ]
+    predicted_prices[i] <- sum(window_data * weights)
+  }
+  
+  return(predicted_prices)
+}
+
+data_to_backtest = train_data
+
+optimal_weights <- matrix(result$par, nrow = window_size, ncol = num_features, byrow = TRUE)
+predicted_test_prices <- predict_prices(data_to_backtest, optimal_weights, window_size)
+
+actual_test_prices <- data_to_backtest$Close_Price[(window_size + 1):nrow(data_to_backtest)]
+mae_test <- mean(abs(actual_test_prices - predicted_test_prices))
+cat("Mean squared Error on Test Data:", mae_test, "\n")
+
+# Create a data frame with the actual and predicted prices
+price_comparison <- data.frame(
+  Date = index(data_to_backtest[(window_size + 1):nrow(data_to_backtest)]),
+  Actual_Price = actual_test_prices,
+  Predicted_Price = predicted_test_prices
+)
+
+# Plot the actual and predicted prices
+ggplot(price_comparison, aes(x = Date)) +
+  geom_line(aes(y = Close_Price, color = "Actual Price")) +
+  geom_line(aes(y = Predicted_Price, color = "Predicted Price")) +
+  labs(title = "Actual vs Predicted Prices",
+       x = "Date",
+       y = "Price",
+       color = "Legend") +
+  theme_minimal()
+
+
+trading_signals <- ifelse(predicted_test_prices > actual_test_prices[-length(actual_test_prices)], "Buy", "Sell")
+
+portfolio_value <- 100000 # Initial portfolio value
+cash <- portfolio_value
+num_shares <- 0
+
+for (i in 1:(length(trading_signals) - 1)) {
+  if (trading_signals[i] == "Buy" && cash >= actual_test_prices[i]) {
+    num_shares_bought <- floor(cash / actual_test_prices[i])
+    num_shares <- num_shares + num_shares_bought
+    cash <- cash - (num_shares_bought * actual_test_prices[i])
+  } else if (trading_signals[i] == "Sell" && num_shares > 0) {
+    cash <- cash + (num_shares * actual_test_prices[i])
+    num_shares <- 0
+  }
+}
+
+# Calculate final portfolio value
+portfolio_value_final <- cash + (num_shares * actual_test_prices[length(actual_test_prices)])
+cat("Initial Portfolio Value:", portfolio_value, "\n")
+cat("Final Portfolio Value:", portfolio_value_final, "\n")
