b714c1c99584d718916ef2a928ee79a86a9572cf
Celia Rubio <celia_rubio@hotmail.es>
2023-04-04 16:39:07 +0100

    Making the code functional + adding majority

--- AlgorithmicTrading/girish_experiments.R
+++ AlgorithmicTrading/girish_experiments.R
@@ -5,75 +5,98 @@
 library(quantmod)
 library(hydroPSO)
 
-getSymbols("^GSPC", src = "yahoo", from = "2018-01-01", to = "2021-12-31")
-sp500 <- Cl(GSPC)
-
-fitness_function <- function(params) {
-  fast = as.integer(params[1])
-  slow = as.integer(params[2])
-  signal = as.integer(params[3])
- 
-  # if(slow - fast <= 5) {
-  #   return(-1e10)
-  # }
-  
-  macd <- MACD(sp500, nFast = fast, nSlow = slow, nSig = signal, maType = "EMA")
-  print(macd)
-  
-  trading_signal <- lag(ifelse(macd$macd > macd$signal, 1, -1))
-  trading_signal[is.na(trading_signal)] <- 0
-  
-  daily_returns <- dailyReturn(sp500)
+get_profit <- function(trading_signal,index) {
+  daily_returns <- dailyReturn(index)
   strategy_returns <- daily_returns * trading_signal
   end_equity <- cumprod(1 + strategy_returns)
   end_profit <- last(end_equity)
-  
-  return(-end_profit)
+  return(end_profit)
 }
 
-lower_bounds <- c(1, 50, 5)
-upper_bounds <- c(50, 200, 50)
+get_signal <- function(params,index) {
+  fast = as.integer(params[1])
+  slow = as.integer(params[2])
+  signal = as.integer(params[3])
+  
+  macd <- MACD(index, nFast = fast, nSlow = slow, nSig = signal, maType = "EMA")
+  print(macd)
+  
+  trading_signal <- lag(ifelse(macd$macd - macd$signal > 0.1, 1, ifelse(macd$macd - macd$signal < -0.1, -1, 0)))
+  trading_signal[is.na(trading_signal)] <- 0
 
-set.seed(123)
-opt_results <- hydroPSO(fn = fitness_function, lower = lower_bounds, upper = upper_bounds, control = list(c1 = 2, c2 = 2, maxit = 100))
+  return(trading_signal)
+}
 
-optimal_params <- as.integer(opt_results$par)
+fitness_function <- function(params,index) {
+  trading_signal <- get_signal(params,index)
+  return(-get_profit(trading_signal,index))
+}
 
-fast_opt <- optimal_params[1]
-slow_opt <- optimal_params[2]
-signal_opt <- optimal_params[3]
+get_optimal_signal <- function(lower_bounds,upper_bounds,index) {
+  this_fitness <- function(params) {return(fitness_function(params,index))}
+  opt_results <- hydroPSO(fn = this_fitness, lower = lower_bounds, upper = upper_bounds, control = list(c1 = 2, c2 = 2, maxit = 100))
+  optimal_params <- as.integer(opt_results$par)
+  return(get_signal(optimal_params,index))
+}
 
-macd_opt <- MACD(sp500, nFast = fast_opt, nSlow = slow_opt, nSig = signal_opt, maType = "EMA")
+get_pool_of_signals <- function(index) {
+  set.seed(42)
+  signal_small <- get_optimal_signal(c(1,5,5),c(5,20,50),index)
+  signal_med <- get_optimal_signal(c(1,15,5),c(15,50,50),index)
+  signal_big <- get_optimal_signal(c(1,50,5),c(50,200,50),index)
+  signal_common <- get_signal(c(12,26,9),index)
 
-trading_signal_opt <- lag(ifelse(macd_opt$macd > macd_opt$signal, 1, -1))
-trading_signal_opt[is.na(trading_signal_opt)] <- 0
+  return(c(signal_small, signal_med, signal_big, signal_common))
+}
 
-daily_returns_opt <- dailyReturn(sp500)
-strategy_returns_opt <- daily_returns_opt * trading_signal_opt
-end_equity_opt <- cumprod(1 + strategy_returns_opt)
+get_majority_signal <- function(pool) {
+  sum <- rowsum(pool)
+  new_signal <- lag(ifelse(sum >= 2, 1, ifelse(sum <= -2, -1, 0)))
+  return(new_signal)
+}
 
-starting_amount <- 10000
-portfolio_value <- starting_amount * end_equity_opt
+# Training data: SPY index from 2018 to 2021
+getSymbols("SPY", src = "yahoo", from = "2018-01-01", to = "2021-12-31")
+training_index <- Cl(SPY)
 
-# Fetching additional data for backtesting
-getSymbols("^GSPC", src = "yahoo", from = "2021-01-01", to = "2021-12-31")
-sp500_test <- Cl(GSPC)
+pool_signals <- get_pool_of_signals(training_index)
 
-# Apply MACD with optimal parameters to test data
-macd_test <- MACD(sp500_test, nFast = fast_opt, nSlow = slow_opt, nSig = signal_opt, maType = "EMA")
+# eval here
 
-# Generate trading signals for test data
-trading_signal_test <- lag(ifelse(macd_test$macd > macd_test$signal, 1, -1))
-trading_signal_test[is.na(trading_signal_test)] <- 0
+majority_signal <- get_majority_signal(pool_signals)
 
-# Calculate daily returns and strategy returns for test data
-daily_returns_test <- dailyReturn(sp500_test)
-strategy_returns_test <- daily_returns_test * trading_signal_test
-end_equity_test <- cumprod(1 + strategy_returns_test)
+# eval here
 
-# Calculate the portfolio value for test data using the starting amount
-portfolio_value_test <- starting_amount * end_equity_test
+all_signals <- c(pool_signals,majority_signal)
 
-# Calculate the performance metrics
-total_return <- last(portfolio_value_test) - starting_amount
-annualized_return <- (total_return / starting_amount) / length(sp500_test) * 252
+print(pool_signals)
+
+# daily_returns_opt <- dailyReturn(sp500)
+# strategy_returns_opt <- daily_returns_opt * trading_signal_opt
+# end_equity_opt <- cumprod(1 + strategy_returns_opt)
+
+# starting_amount <- 10000
+# portfolio_value <- starting_amount * end_equity_opt
+
+# # Fetching additional data for backtesting
+# getSymbols("^GSPC", src = "yahoo", from = "2021-01-01", to = "2021-12-31")
+# sp500_test <- Cl(GSPC)
+
+# # Apply MACD with optimal parameters to test data
+# macd_test <- MACD(sp500_test, nFast = fast_opt, nSlow = slow_opt, nSig = signal_opt, maType = "EMA")
+
+# # Generate trading signals for test data
+# trading_signal_test <- lag(ifelse(macd_test$macd > macd_test$signal, 1, -1))
+# trading_signal_test[is.na(trading_signal_test)] <- 0
+
+# # Calculate daily returns and strategy returns for test data
+# daily_returns_test <- dailyReturn(sp500_test)
+# strategy_returns_test <- daily_returns_test * trading_signal_test
+# end_equity_test <- cumprod(1 + strategy_returns_test)
+
+# # Calculate the portfolio value for test data using the starting amount
+# portfolio_value_test <- starting_amount * end_equity_test
+
+# # Calculate the performance metrics
+# total_return <- last(portfolio_value_test) - starting_amount
+# annualized_return <- (total_return / starting_amount) / length(sp500_test) * 252
